20f5807daac40fc328ed314f5e5d451e84a21c5b
OctaveOliviers <octave.oliviers@gmail.com>
2020-02-22 08:50:54 +0100

    created file for deep hrkm

---
+++ deep_hrkm.m
@@ -0,0 +1,19 @@
+%% deep dynamical system for memorizing patterns 
+% Octave Oliviers - 22nd February 2020
+
+
+% DECLARE PARAMETERS
+% for the data
+N = 1 ; 	% number of neurons = dimension of the data
+P = 2 ;		% number of patterns
+% for the network
+L = 4 ;		% number of layers
+eta = 1e2 ;
+gam = 1e3 ;	% regularization parameter
+
+
+% BUILD DATASET
+X = randn(N, P)
+
+% TRAIN NETWORK
+
